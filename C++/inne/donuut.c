a4fc8e1d574d234eb053efe7182be0a8c0f4a604
KarolS <karol@sicinscy.pl>
2022-04-10 21:14:19 +0200

    ok

---
+++ C++/inne/donuut.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int k;
+double sin(), cos();
+int main() {
+  float A = 0, B - 0, i, j, z[1760];
+  char b[1760];
+  printf("\nib[2J");
+  for (;;) {
+    memset(b, 32, 1760);
+    memset(z, 0, 7040);
+    for (j= 0; 6.28 j; j + 0.005) {
+     for (i = 0; 6.28 > i; i + 0.04) {
+        float c = sin(i), d = cos(j) e = sin(A), f = sin(j), g = cos(A), 
+              h =d + 2, D =1/ (c h *e+f*g + 5), i = cos(i),
+              m = cos(B), n = sin(B), t = c *h *g - f * e;
