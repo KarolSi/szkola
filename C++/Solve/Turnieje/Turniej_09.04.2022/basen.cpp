58054426ac22c6fd3d707acf06103ef2989731ce
KarolS <karol@sicinscy.pl>
2022-04-10 21:14:01 +0200

    costam

---
+++ C++/Solve/Turnieje/Turniej_09.04.2022/basen.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long q, n;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>q;
+    for (int i = 0; i < q; i++)
+    {
+        cin>>n;
+        if(n<5)
+        {
+            cout<<"NIE\n";
+            break;
+        }
+        
+
+
+
+    }
+    return 0;
+}
+
+
