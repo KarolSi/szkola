47c412a20e19949e13cdf23fe84ce57ebc911bd3
KarolSi <karol@sicinscy.pl>
2022-05-09 08:22:11 -0400

    testowy z linucha

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+string h = "Heloo world";
+long long y = 1e10+45;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<h<<"\n"<<y<<"\n";
+    }
+    return 0;
+}
